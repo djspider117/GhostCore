f4337e2930a4362adab3ccd2179a728454c0d587
Ionut Popovici <ionut.popovici@icoldo.com>
2017-09-21 17:16:47 +0300

    added some win32 apis

---
+++ GhostCore/Win32/GDI32.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace GhostCore.Win32
+{
+    public static class GDI32
+    {
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+    }
+}
---
+++ GhostCore/Win32/Kernel32.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace GhostCore.Win32
+{
+    public static class Kernel32
+    {
+        [DllImport("kernel32.dll")]
+        public static extern void RtlMoveMemory(IntPtr dest, IntPtr source, int Length);
+
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
+
+        [DllImport("Kernel32")]
+        public static extern void FreeConsole();
+    }
+}
