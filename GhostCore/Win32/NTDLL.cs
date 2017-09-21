60d218ba0ed5fb8e391e34f9bad2811f24571eb1
Ionut Popovici <ionut.popovici@icoldo.com>
2017-09-21 17:38:27 +0300

    added ntdll.dll interop

---
+++ GhostCore/Win32/NTDLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace GhostCore.Win32
+{
+    public static class NTDLL
+    {
+        [DllImport("ntdll.dll", SetLastError = false)]
+        internal static extern IntPtr NtSuspendProcess(IntPtr ProcessHandle);
+
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        internal static extern IntPtr NtResumeProcess(IntPtr ProcessHandle);
+    }
+}
