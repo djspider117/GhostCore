98eb9a88890e5b18bc74e0922d667da424b44643
Ionut Popovici <ionut.popovici@icoldo.com>
2017-09-21 17:43:05 +0300

    process suspender

---
+++ GhostCore/Utility/ProcessSuspender.cs
@@ -0,0 +1,40 @@
+﻿using GhostCore.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace GhostCore.Utility
+{
+    public static class ProcessSuspender
+    {
+        public static void SuspendProcess(IntPtr handle)
+        {
+            NTDLL.NtSuspendProcess(handle);
+        }
+
+        public static void SuspendProcess(string procName)
+        {
+            var proc = Process.GetProcessesByName(procName)?.FirstOrDefault();
+            if (proc == null)
+                return;
+
+            NTDLL.NtSuspendProcess(proc.Handle);
+        }
+
+        public static void ResumeProcess(IntPtr handle)
+        {
+            NTDLL.NtResumeProcess(handle);
+        }
+
+        public static void ResumeProcess(string procName)
+        {
+            var proc = Process.GetProcessesByName(procName)?.FirstOrDefault();
+            if (proc == null)
+                return;
+
+            NTDLL.NtResumeProcess(proc.Handle);
+        }
+    }
+}
