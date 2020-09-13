88e472ed57bb66e495f6cf8440b8becfe3db5492
John Popovici <ionut.popovici@engageworks.com>
2020-09-14 01:39:39 +0300

    Implemented some standard sigmoid curves to use in interpolation

---
+++ GhostCore/Numerics/Interpolation/CosineSCurve.cs
@@ -0,0 +1,17 @@
+﻿using SysMath = System.Math;
+
+namespace GhostCore.Numerics.Interpolation
+{
+    ///<summary>
+    /// Cosine interpolation uses cosine function instead of power curve, resulting in somewhat smoother noise than cubic interpolation, but still only achieving first-order continuity.
+    /// Depending on target machine, it may be faster than quintic interpolation.
+    ///</summary>
+    public class CosineSCurve : SCurve
+    {
+        /// <inheritdoc/>
+        public override float GetValue(float t)
+        {
+            return (float)((1 - SysMath.Cos(t * SysMath.PI)) * .5);
+        }
+    }
+}
---
+++ GhostCore/Numerics/Interpolation/CubicSCurve.cs
@@ -0,0 +1,14 @@
+﻿namespace GhostCore.Numerics.Interpolation
+{
+    ///<summary>
+    /// Cubic interpolation is a good compromise between speed and quality. It's slower than linear, but ensures continuity of 1-st order derivatives, making noise smooth.
+    ///</summary>
+    public class CubicSCurve : SCurve
+    {
+        /// <inheritdoc />
+        public override float GetValue(float t)
+        {
+            return t * t * (3f - 2f * t);
+        }
+    }
+}
