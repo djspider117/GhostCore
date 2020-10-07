ad190527316569df7b24d4b3ffa68d2cd6a140bb
Arlodotexe <arlo.godfrey@outlook.com>
2020-10-07 07:47:03 -0500

    Added RegexUtility and email tester

---
+++ GhostCore/Utility/RegexUtilities.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace GhostCore.Utility
+{
+    public static class RegexUtilities
+    {
+        /// <summary>
+        /// Checks if the inputted string is a valid email.
+        /// </summary>
+        /// <param name="email">The string to test.</param>
+        /// <returns><see landword="true"/> if the string is an email, otherwise <see langword="false"/>.</returns>
+        /// <remarks>Source: <see href="https://docs.microsoft.com/en-us/dotnet/standard/base-types/how-to-verify-that-strings-are-in-valid-email-format"/>.</remarks>
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                    RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+}
