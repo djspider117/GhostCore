36a7e72c9c360ebb6623d32f54554a268c04180d
John Popovici <ionut.popovici@engageworks.com>
2020-10-08 13:38:38 +0300

    Added StringFormatConverter

---
+++ GhostCore.UWP/Converters/StringFormatConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace GhostCore.UWP.Converters
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public string StringFormat { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!string.IsNullOrEmpty(StringFormat) && value != null)
+                return string.Format(StringFormat, value);
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
