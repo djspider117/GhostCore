b88e150f4fc8483eb0738d2460de4cd4d5f6295d
John Popovici <ionut.popovici@engageworks.com>
2020-10-22 11:49:07 +0300

    Added BoolToDoubleConverter.cs

---
+++ GhostCore.UWP/Converters/BoolToDoubleConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace GhostCore.UWP.Converters
+{
+    public class BoolToDoubleConverter : IValueConverter
+    {
+        public double TrueValue { get; set; } = 1;
+        public double FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool b)
+            {
+                if (b) 
+                    return TrueValue;
+
+                return FalseValue;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is double d)
+            {
+                if (d == TrueValue)
+                    return true;
+
+                return false;
+            }
+
+            return value;
+        }
+    }
+}
