365ae1d3a94784e45ae8a0dc1b1534fed4527e7c
John Popovici <ionut.popovici@engageworks.com>
2020-10-30 20:08:25 +0200

    Added ClipToBounds and UserControlEx

---
+++ GhostCore.UWP/Helpers/ClipToBounds.cs
@@ -0,0 +1,62 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace GhostCore.UWP.Helpers
+{
+    public class ClipToBounds
+    {
+        public static readonly DependencyProperty ClipToBoundsProperty =
+            DependencyProperty.RegisterAttached("ClipToBounds", typeof(bool), typeof(ClipToBounds), new PropertyMetadata(false, ClipToBoundsChanged));
+
+        public static bool GetClipToBounds(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(ClipToBoundsProperty);
+        }
+
+        public static void SetClipToBounds(DependencyObject obj, bool value)
+        {
+            obj.SetValue(ClipToBoundsProperty, value);
+        }
+
+        private static void ClipToBoundsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //use FrameworkElement because it is the highest abstraction that contains safe size
+            //UIElement does not contain save size data
+            var element = d as FrameworkElement;
+            if (element != null)
+            {
+                element.Loaded += Element_Loaded;
+                element.SizeChanged += Element_SizeChanged;
+                element.Unloaded += Element_Unloaded;
+            }
+        }
+        private static void Element_Loaded(object sender, RoutedEventArgs e)
+        {
+            ClipElement(sender as FrameworkElement);
+        }
+
+        private static void Element_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ClipElement(sender as FrameworkElement);
+        }
+
+        private static void Element_Unloaded(object sender, RoutedEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+
+            element.SizeChanged -= Element_SizeChanged;
+            element.Unloaded -= Element_Unloaded;
+        }
+
+        private static void ClipElement(FrameworkElement element)
+        {
+            if (GetClipToBounds(element))
+            {
+                var clip = new RectangleGeometry { Rect = new Rect(0, 0, element.ActualWidth, element.ActualHeight) };
+                element.Clip = clip;
+            }
+        }
+    }
+
+}
---
+++ GhostCore.UWP/UserControlEx.cs
@@ -0,0 +1,62 @@
+﻿using GhostCore.Services.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace GhostCore.UWP
+{
+    public class UserControlEx : UserControl
+    {
+        private bool _isLoaded;
+
+        public UserControlEx()
+        {
+            Loaded += UserControlEx_Loaded;
+            Unloaded += UserControlEx_Unloaded;
+        }
+
+        private async void UserControlEx_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= UserControlEx_Loaded;
+            Unloaded -= UserControlEx_Unloaded;
+
+            try
+            {
+                await OnUnloaded(sender, e);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+                throw;
+            }
+        }
+
+        private async void UserControlEx_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await OnLoaded(sender, e, _isLoaded);
+                _isLoaded = true;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+                throw;
+            }
+        }
+
+        protected virtual Task OnLoaded(object sender, RoutedEventArgs e, bool wasLoadedPreviously)
+        {
+            return Task.CompletedTask;
+        }
+
+        protected virtual Task OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
