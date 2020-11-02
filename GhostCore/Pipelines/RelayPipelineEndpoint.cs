197d59f292d6db2b9212f9167257886adf885773
John Popovici <ionut.popovici@engageworks.com>
2020-11-02 19:18:57 +0200

    Added RelayPipelineEndpoint

---
+++ GhostCore/Pipelines/RelayPipelineEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GhostCore.Pipelines
+{
+    public class RelayPipelineEndpoint : IPipelineEndpoint
+    {
+        private Func<PipelineProcessData, Task> _action;
+
+        public RelayPipelineEndpoint(Func<PipelineProcessData, Task> action)
+        {
+            _action = action;
+        }
+
+        public Task FinishPipeline(PipelineProcessData pipelineProcessData)
+        {
+            return _action(pipelineProcessData);
+        }
+    }
+}
