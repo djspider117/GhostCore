e829647d93f0207644a3b3b43117c49d2499ea6b
John Popovici <ionut.popovici@engageworks.com>
2020-11-05 13:15:15 +0200

    Added IAsyncCloneable

---
+++ GhostCore/IAsyncCloneable.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace GhostCore
+{
+    /// <summary>
+    /// Supports cloning asynchronously, which creates a new instance of a class with the same value as an existing instance.
+    /// </summary>
+    public interface IAsyncCloneable
+    {
+        Task<T> CloneAsync<T>();
+
+        Task<object> CloneAsync();
+    }
+}
