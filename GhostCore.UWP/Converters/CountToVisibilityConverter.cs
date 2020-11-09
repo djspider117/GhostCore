f0bdd1ebf2c41a67145d710b16a6c32f325e39c1
John Popovici <ionut.popovici@engageworks.com>
2020-11-09 17:38:01 +0200

    wip support for xml serialization

---
+++ GhostCore.UWP/Converters/CountToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace GhostCore.UWP.Converters
+{
+    public class CountToVisibilityConverter : IValueConverter
+    {
+        public int CollapseUnder { get; set; }
+        public Visibility DefaultVisibility { get; set; } = Visibility.Visible;
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int i)
+            {
+                return i <= CollapseUnder ? Visibility.Collapsed : Visibility.Visible;
+            }
+
+            return DefaultVisibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- GhostCore/Numerics/TransformData.cs
+++ GhostCore/Numerics/TransformData.cs
@@ -3,15 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace GhostCore.Numerics
 {
     public class TransformData
     {
+        [XmlAttribute]
         public double TranslateX { get; set; }
+
+        [XmlAttribute]
         public double TranslateY { get; set; }
+
+        [XmlAttribute]
         public double Rotation { get; set; }
+
+        [XmlAttribute]
         public double ScaleX { get; set; } = 1;
+
+        [XmlAttribute]
         public double ScaleY { get; set; } = 1;
     }
 
