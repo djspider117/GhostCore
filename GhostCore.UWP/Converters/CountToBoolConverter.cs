5bd71d5e7bf46ee9c9a93233f0b22e425e4d4294
Arlodotexe <arlo.godfrey@outlook.com>
2020-11-16 08:07:45 -0600

    Added CountToBoolConverter

---
+++ GhostCore.UWP/Converters/CountToBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace GhostCore.UWP.Converters
+{
+    public class CountToBoolConverter : IValueConverter
+    {
+        public int TrueWhenOver { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int i)
+            {
+                return i > TrueWhenOver;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
