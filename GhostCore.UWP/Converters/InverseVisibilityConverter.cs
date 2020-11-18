b152058669f7be29479b875daffef1556f6f72bf
John Popovici <ionut.popovici@engageworks.com>
2020-11-18 11:31:09 +0200

    Added InverseVisibilityConverter

---
+++ GhostCore.UWP/Converters/InverseVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace GhostCore.UWP.Converters
+{
+    public class InverseVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Visibility visibility)
+                return visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
