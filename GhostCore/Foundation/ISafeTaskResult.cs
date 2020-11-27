55bbe69b9405f8810c2a8d8c1c35fc435fccc607
John Popovici <ionut.popovici@engageworks.com>
2020-11-27 12:52:30 +0200

    Added special case for string results for SafeTaskResult (as it conflicts with the error constructor)

--- GhostCore/Foundation/ISafeTaskResult.cs
+++ GhostCore/Foundation/ISafeTaskResult.cs
@@ -93,4 +93,34 @@ namespace GhostCore.Foundation
         }
     }
 
+    public class StringSafeTaskResult : SafeTaskResult, ISafeTaskResult<string>
+    {
+        public string ResultValue { get; }
+
+        public StringSafeTaskResult(string resultValue)
+        {
+            ResultValue = resultValue;
+        }
+
+        public StringSafeTaskResult(bool isError, string failReason) : base(failReason)
+        {
+        }
+
+        public StringSafeTaskResult(bool isError, string failReason, Exception ex) : base(failReason, ex)
+        {
+        }
+
+        public StringSafeTaskResult(bool isError, string failReason, int hresult) : base(failReason, hresult)
+        {
+        }
+
+        public StringSafeTaskResult(bool isError, string failReason, Exception ex, int hresult) : base(failReason, ex, hresult)
+        {
+        }
+
+        public ISafeTaskResult<K> Cast<K>()
+        {
+            return this as ISafeTaskResult<K>;
+        }
+    }
 }
