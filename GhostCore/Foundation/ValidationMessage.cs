d7b2b36137457d9b810aae7297de786accd5641e
Ionut Popovici <ionut.popovici@engageworks.com>
2021-02-18 13:42:08 +0200

    Added ValidationCollection and ValidationMessage

---
+++ GhostCore/Foundation/ValidationMessage.cs
@@ -0,0 +1,36 @@
+﻿namespace GhostCore.Foundation
+{
+    /// <summary>
+    /// Provides data for error handling after content validation
+    /// </summary>
+    public class ValidationMessage
+    {
+        /// <summary>
+        /// True if this message constitute an error, false for warnings or information. 
+        /// </summary>
+        public bool IsError { get; set; }
+
+        /// <summary>
+        /// The message
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Creates an instance of a <see cref="ValidationMessage"/>
+        /// </summary>
+        /// <param name="msg">The message</param>
+        public ValidationMessage(string msg)
+        {
+            Message = msg;
+        }
+        /// <summary>
+        /// Creates an instance of a <see cref="ValidationMessage"/>
+        /// </summary>
+        /// <param name="msg">The message</param>
+        /// <param name="isError">True if error, false otherwise</param>
+        public ValidationMessage(bool isError, string msg) : this(msg)
+        {
+            IsError = isError;
+        }
+    }
+}
