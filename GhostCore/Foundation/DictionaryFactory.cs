eee338912d0248eb0850ff5925c2544bb3834501
Ionut Popovici <ionut.popovici@engageworks.com>
2021-02-19 14:48:03 +0200

    Added DictionaryFactory abstract class to help speed up basic factory creation.

---
+++ GhostCore/Foundation/DictionaryFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GhostCore.Foundation
+{
+    public abstract class DictionaryFactory<TFactoryKey, TBaseClass>
+    {
+        private Dictionary<TFactoryKey, Func<TBaseClass>> _factoryMap;
+
+        public DictionaryFactory()
+        {
+            _factoryMap = new Dictionary<TFactoryKey, Func<TBaseClass>>();
+            Configure(_factoryMap);
+        }
+
+        protected abstract void Configure(Dictionary<TFactoryKey, Func<TBaseClass>> factoryMap);
+
+        public TBaseClass Create(TFactoryKey factoryKey)
+        {
+            if (!_factoryMap.ContainsKey(factoryKey))
+                return default;
+
+            return _factoryMap[factoryKey]();
+        }
+    }
+}
