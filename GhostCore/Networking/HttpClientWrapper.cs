c7d59bea7e972109d505041466f678b8d569a7c5
Ionut Popovici <ionut.popovici@engageworks.com>
2021-02-22 15:28:12 +0200

    Refactored RestProxy to use HttpClientWrapper that adds the ability to ignore invalid certificates on HTTPS

---
+++ GhostCore/Networking/HttpClientWrapper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GhostCore.Networking
+{
+    internal class HttpClientWrapper : IDisposable
+    {
+        public HttpClient Client { get; internal set; }
+        public HttpClientHandler Handler { get; internal set; }
+
+        public HttpClientWrapper(HttpClient cli, HttpClientHandler handler = null)
+        {
+            Client = cli ?? throw new ArgumentNullException(nameof(cli));
+            Handler = handler;
+        }
+
+        public Task<HttpResponseMessage> DeleteAsync(Uri requestUri, CancellationToken cancellationToken) => Client.DeleteAsync(requestUri, cancellationToken);
+        public Task<HttpResponseMessage> DeleteAsync(string requestUri, CancellationToken cancellationToken) => Client.DeleteAsync(requestUri, cancellationToken);
+        public Task<HttpResponseMessage> DeleteAsync(string requestUri) => Client.DeleteAsync(requestUri);
+        public Task<HttpResponseMessage> DeleteAsync(Uri requestUri) => Client.DeleteAsync(requestUri);
+
+        public Task<HttpResponseMessage> GetAsync(Uri requestUri) => Client.GetAsync(requestUri);
+        public Task<HttpResponseMessage> GetAsync(string requestUri) => Client.GetAsync(requestUri);
+        public Task<HttpResponseMessage> GetAsync(string requestUri, HttpCompletionOption completionOption) => Client.GetAsync(requestUri, completionOption);
+        public Task<HttpResponseMessage> GetAsync(string requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken) => Client.GetAsync(requestUri, completionOption, cancellationToken);
+        public Task<HttpResponseMessage> GetAsync(string requestUri, CancellationToken cancellationToken) => Client.GetAsync(requestUri, cancellationToken);
+
+        public Task<HttpResponseMessage> GetAsync(Uri requestUri, HttpCompletionOption completionOption) => Client.GetAsync(requestUri, completionOption);
+        public Task<HttpResponseMessage> GetAsync(Uri requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken) => Client.GetAsync(requestUri, completionOption, cancellationToken);
+        public Task<HttpResponseMessage> GetAsync(Uri requestUri, CancellationToken cancellationToken) => Client.GetAsync(requestUri, cancellationToken);
+
+        public Task<byte[]> GetByteArrayAsync(string requestUri) => Client.GetByteArrayAsync(requestUri);
+        public Task<byte[]> GetByteArrayAsync(Uri requestUri) => Client.GetByteArrayAsync(requestUri);
+
+        public Task<Stream> GetStreamAsync(string requestUri) => Client.GetStreamAsync(requestUri);
+        public Task<Stream> GetStreamAsync(Uri requestUri) => Client.GetStreamAsync(requestUri);
+
+        public Task<string> GetStringAsync(string requestUri) => Client.GetStringAsync(requestUri);
+        public Task<string> GetStringAsync(Uri requestUri) => Client.GetStringAsync(requestUri);
+
+        public Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content) => Client.PostAsync(requestUri, content);
+        public Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content, CancellationToken cancellationToken) => Client.PostAsync(requestUri, content, cancellationToken);
+        public Task<HttpResponseMessage> PostAsync(Uri requestUri, HttpContent content) => Client.PostAsync(requestUri, content);
+        public Task<HttpResponseMessage> PostAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken) => Client.PostAsync(requestUri, content, cancellationToken);
+
+        public Task<HttpResponseMessage> PutAsync(string requestUri, HttpContent content) => Client.PutAsync(requestUri, content);
+        public Task<HttpResponseMessage> PutAsync(string requestUri, HttpContent content, CancellationToken cancellationToken) => Client.PutAsync(requestUri, content, cancellationToken);
+        public Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content) => Client.PutAsync(requestUri, content);
+        public Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken) => Client.PutAsync(requestUri, content, cancellationToken);
+
+        public void Dispose()
+        {
+            if (Client != null)
+            {
+                Client.CancelPendingRequests();
+                Client.Dispose();
+            }
+
+            if (Handler != null)
+            {
+                Handler.Dispose();
+            }
+        }
+    }
+}
--- GhostCore/Networking/RestProxy.cs
+++ GhostCore/Networking/RestProxy.cs
@@ -1,7 +1,6 @@
 ﻿using GhostCore;
 using GhostCore.Foundation;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.NetworkInformation;
@@ -12,13 +11,24 @@ namespace GhostCore.Networking
 {
     public class RestProxy
     {
+        #region Constants
+
         public const int SERVER_PING_TIMEOUT_MS = 60 * 1000;
+
+        #endregion
+
+        #region Fields
 
         protected string _baseUrl;
         protected string _urlOverride;
         protected string _endpoint;
 
+        #endregion
+
+        #region Properties
+
         public string LastErrorMessage { get; set; }
+        public bool IgnoreSSL { get; set; }
 
         public RestProxyAuthenicationHandler AuthenticationHandler { get; set; }
 
@@ -32,6 +42,10 @@ namespace GhostCore.Networking
             }
         }
 
+        #endregion
+
+        #region Constructors and Initialization
+
         public RestProxy() : this(null, null, new RestProxyAuthenicationHandler())
         {
 
@@ -40,7 +54,6 @@ namespace GhostCore.Networking
         public RestProxy(string baseUrl, string endpoint)
             : this(baseUrl, endpoint, new RestProxyAuthenicationHandler())
         {
-
         }
 
         public RestProxy(string baseUrl, string endpoint, RestProxyAuthenicationHandler authHandler)
@@ -49,6 +62,12 @@ namespace GhostCore.Networking
             _endpoint = endpoint;
             AuthenticationHandler = authHandler;
         }
+
+
+        #endregion
+
+        #region Helpers
+
 
         public HttpContent CreateHttpContent(object data)
         {
@@ -71,19 +90,46 @@ namespace GhostCore.Networking
             _urlOverride = fullUrl.Substring(0, idx);
         }
 
-
         protected void SetError(string data)
         {
             var errorData = JsonConvert.DeserializeObject<ErrorResponse>(data);
             LastErrorMessage = errorData.ServerMessage;
         }
 
+        #endregion
+
+        #region HttpClient Factory
+
+        internal HttpClientWrapper MakeHttpClientWrapper(bool ignoreSSL = false)
+        {
+            if (ignoreSSL)
+            {
+                var handler = new HttpClientHandler
+                {
+                    ClientCertificateOptions = ClientCertificateOption.Manual,
+                    ServerCertificateCustomValidationCallback = (httpRequestMessage, cert, cetChain, policyErrors) => true
+                };
+
+                var cli = new HttpClient(handler);
+                return new HttpClientWrapper(cli, handler);
+            }
+            else
+            {
+                var cli = new HttpClient();
+                return new HttpClientWrapper(cli);
+            }
+        }
+
+        #endregion
+
+        #region API
+
         protected async Task<ISafeTaskResult> SafePostAsync(string url, object req, bool useAuth = false)
         {
-            using var cli = new HttpClient();
+            using var cli = MakeHttpClientWrapper(IgnoreSSL);
 
             if (useAuth)
-                await AuthenticationHandler.AddAuthenticationHeader(cli);
+                await AuthenticationHandler.AddAuthenticationHeader(cli.Client);
 
             var content = CreateHttpContent(req);
             string requestUri = url;
@@ -106,12 +152,10 @@ namespace GhostCore.Networking
         }
         protected async Task<ISafeTaskResult<T>> SafePostAsync<T>(string url, object req, bool useAuth = false)
         {
-            using var cli = new HttpClient();
+            using var cli = MakeHttpClientWrapper(IgnoreSSL);
 
             if (useAuth)
-            {
-                await AuthenticationHandler.AddAuthenticationHeader(cli);
-            }
+                await AuthenticationHandler.AddAuthenticationHeader(cli.Client);
 
             var content = CreateHttpContent(req);
             string requestUri = url;
@@ -138,10 +182,10 @@ namespace GhostCore.Networking
 
         protected async Task<ISafeTaskResult> SafeGetAsync(string url, bool useAuth = false)
         {
-            using var cli = new HttpClient();
+            using var cli = MakeHttpClientWrapper(IgnoreSSL);
 
             if (useAuth)
-                await AuthenticationHandler.AddAuthenticationHeader(cli);
+                await AuthenticationHandler.AddAuthenticationHeader(cli.Client);
 
             var httpResponse = await cli.GetAsync(url);
 
@@ -162,12 +206,10 @@ namespace GhostCore.Networking
         }
         protected async Task<ISafeTaskResult<T>> SafeGetAsync<T>(string url, bool useAuth = false)
         {
-            using var cli = new HttpClient();
+            using var cli = MakeHttpClientWrapper(IgnoreSSL);
 
             if (useAuth)
-            {
-                await AuthenticationHandler.AddAuthenticationHeader(cli);
-            }
+                await AuthenticationHandler.AddAuthenticationHeader(cli.Client);
 
             var httpResponse = await cli.GetAsync(url);
 
@@ -193,12 +235,10 @@ namespace GhostCore.Networking
 
         protected async Task<T> PostAsync<T>(string url, object req, bool useAuth = false)
         {
-            var cli = new HttpClient();
+            using var cli = MakeHttpClientWrapper(IgnoreSSL);
 
             if (useAuth)
-            {
-                await AuthenticationHandler.AddAuthenticationHeader(cli);
-            }
+                await AuthenticationHandler.AddAuthenticationHeader(cli.Client);
 
             var content = CreateHttpContent(req);
             string requestUri = url;
@@ -229,12 +269,10 @@ namespace GhostCore.Networking
 
         protected async Task<T> GetAsync<T>(string url, bool useAuth = false)
         {
-            var cli = new HttpClient();
+            using var cli = MakeHttpClientWrapper(IgnoreSSL);
 
             if (useAuth)
-            {
-                await AuthenticationHandler.AddAuthenticationHeader(cli);
-            }
+                await AuthenticationHandler.AddAuthenticationHeader(cli.Client);
 
             var httpResponse = await cli.GetAsync(url);
 
@@ -264,12 +302,10 @@ namespace GhostCore.Networking
 
         protected async Task<byte[]> GetRawAsync(string url, bool useAuth = false)
         {
-            var cli = new HttpClient();
+            using var cli = MakeHttpClientWrapper(IgnoreSSL);
 
             if (useAuth)
-            {
-                await AuthenticationHandler.AddAuthenticationHeader(cli);
-            }
+                await AuthenticationHandler.AddAuthenticationHeader(cli.Client);
 
             var httpResponse = await cli.GetAsync(url);
 
@@ -284,14 +320,12 @@ namespace GhostCore.Networking
                 {
                     LastErrorMessage = httpResponse.StatusCode.ToString();
                 }
-                cli.Dispose();
                 return null;
             }
             else
             {
                 var data = await httpResponse.Content.ReadAsByteArrayAsync();
 
-                cli.Dispose();
                 return data;
             }
         }
@@ -300,6 +334,8 @@ namespace GhostCore.Networking
         {
             return Task.FromResult(NetworkInterface.GetIsNetworkAvailable());
         }
+
+        #endregion
     }
 
     public class RestProxyAuthenicationHandler
