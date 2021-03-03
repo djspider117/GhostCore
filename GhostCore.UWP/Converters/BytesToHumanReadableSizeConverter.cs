205841f2c5a1249ccd2289e831359d2a5fefa52e
Arlodotexe <arlo.godfrey@outlook.com>
2021-03-03 03:59:52 -0600

    Fixed converter base type

---
+++ GhostCore.UWP/Converters/BytesToHumanReadableSizeConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace GhostCore.UWP.Converters
+{
+    public class BytesToHumanReadableSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                string[] suffixNames = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+                var counter = 0;
+
+                decimal.TryParse(value.ToString(), out var dValue);
+
+                while (Math.Round(dValue / 1024) >= 1)
+                {
+                    dValue /= 1024;
+                    counter++;
+                }
+
+                return $"{dValue:n1} {suffixNames[counter]}";
+            }
+            catch
+            {
+                //catch and handle the exception
+                return string.Empty;
+            }
+		}
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- GhostCore.UWP/Converters/NullOrEmptyStringToVisibilityConverter.cs
+++ GhostCore.UWP/Converters/NullOrEmptyStringToVisibilityConverter.cs
@@ -1,10 +1,10 @@
-﻿using GhostCore.ObjectModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
 
 namespace GhostCore.UWP.Converters
 {
