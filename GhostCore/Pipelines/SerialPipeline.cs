1b0328e5041397a8c54f22e5de2b3b2f25cbe7c7
Ionut Popovici <ionut.popovici@engageworks.com>
2021-03-09 13:32:27 +0200

    Fixed an issue on SerialPipeline where the PipelineStart was set after the processors were executed and not before. Added IRequestReload.cs

--- GhostCore/Pipelines/SerialPipeline.cs
+++ GhostCore/Pipelines/SerialPipeline.cs
@@ -35,6 +35,7 @@ namespace GhostCore.Pipelines
             }
 
             _pdata = CreatePipelineProcessData(sourceObject, pipelineArguments);
+            _pdata.PipelineStarter = sender;
 
             LogPipelineMessage("Starting processors...", LoggingLevel.Verbose);
             foreach (var ipp in _processors)
@@ -64,7 +65,6 @@ namespace GhostCore.Pipelines
             }
 
             await ProcessEndpoint(_pdata);
-            _pdata.PipelineStarter = sender;
 
             FinishPipeline(_pdata);
             _pdata = null;
