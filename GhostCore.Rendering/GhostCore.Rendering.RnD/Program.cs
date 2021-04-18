500bd9be35c2e4519937c416f327d93f1be12039
Ionut Popovici <ionut.popovici@engageworks.com>
2021-04-18 18:34:15 +0300

    initial commit

---
+++ GhostCore.Rendering/GhostCore.Rendering.RnD/Program.cs
@@ -0,0 +1,220 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+using System;
+
+namespace GhostCore.Rendering.RnD
+{
+    unsafe class Program
+    {
+        private static IWindow window;
+        private static GL Gl;
+
+        private static uint Vbo;
+        private static uint Ebo;
+        private static uint Vao;
+        private static uint Shader;
+
+        //Vertex shaders are run on each vertex.
+        private static readonly string VertexShaderSource = @"
+        #version 330 core //Using version GLSL version 3.3
+        layout (location = 0) in vec4 vPos;
+        
+        void main()
+        {
+            gl_Position = vec4(vPos.x, vPos.y, vPos.z, 1.0);
+        }
+        ";
+
+        //Fragment shaders are run on each fragment/pixel of the geometry.
+        private static readonly string FragmentShaderSource = @"
+        #version 330 core
+        out vec4 FragColor;
+
+        void main()
+        {
+            FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+        }
+        ";
+
+        //Vertex data, uploaded to the VBO.
+        private static readonly float[] Vertices =
+        {
+            //X    Y      Z
+             0.5f,  0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+            -0.5f,  0.5f, 0.5f
+        };
+
+        //Index data, uploaded to the EBO.
+        private static readonly uint[] Indices =
+        {
+            0, 1, 3,
+            1, 2, 3
+        };
+
+
+        static void Main(string[] args)
+        {
+            //Create a window.
+            var options = WindowOptions.Default;
+            options.Size = new Vector2D<int>(800, 600);
+            options.Title = "OpenGL#";
+
+            window = Window.Create(options);
+
+            //Assign events.
+            window.Load += OnLoad;
+            window.Update += OnUpdate;
+            window.Render += OnRender;
+            window.Closing += OnClose;
+
+            //Run the window.
+            window.Run();
+        }
+
+        private unsafe static void OnLoad()
+        {
+            //Set-up input context.
+            IInputContext input = window.CreateInput();
+            for (int i = 0; i < input.Keyboards.Count; i++)
+            {
+                input.Keyboards[i].KeyDown += KeyDown;
+            }
+
+            Gl = GL.GetApi(window);
+
+            //Creating a vertex array.
+            Vao = Gl.GenVertexArray();
+            Gl.BindVertexArray(Vao);
+
+            InitializeVBO();
+            InitializeEBO();
+
+            uint vertexShader = BuildAndCompileVertexShader();
+            uint fragmentShader = BuildAndCompileFragmentShader();
+
+            //Combining the shaders under one shader program.
+            MergeShaders(vertexShader, fragmentShader);
+
+            //Tell opengl how to give the data to the shaders.
+            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), null);
+            Gl.EnableVertexAttribArray(0);
+        }
+
+        #region Init
+
+        private static unsafe void MergeShaders(uint vertexShader, uint fragmentShader)
+        {
+            Shader = Gl.CreateProgram();
+            Gl.AttachShader(Shader, vertexShader);
+            Gl.AttachShader(Shader, fragmentShader);
+            Gl.LinkProgram(Shader);
+
+            //Checking the linking for errors.
+            Gl.GetProgram(Shader, GLEnum.LinkStatus, out var status);
+            if (status == 0)
+            {
+                Console.WriteLine($"Error linking shader {Gl.GetProgramInfoLog(Shader)}");
+            }
+
+            //Delete the no longer useful individual shaders;
+            Gl.DetachShader(Shader, vertexShader);
+            Gl.DetachShader(Shader, fragmentShader);
+            Gl.DeleteShader(vertexShader);
+            Gl.DeleteShader(fragmentShader);
+        }
+        private static unsafe uint BuildAndCompileFragmentShader()
+        {
+            uint fragmentShader = Gl.CreateShader(ShaderType.FragmentShader);
+            Gl.ShaderSource(fragmentShader, FragmentShaderSource);
+            Gl.CompileShader(fragmentShader);
+
+            //Checking the shader for compilation errors.
+            var infoLog = Gl.GetShaderInfoLog(fragmentShader);
+            if (!string.IsNullOrWhiteSpace(infoLog))
+            {
+                Console.WriteLine($"Error compiling fragment shader {infoLog}");
+            }
+
+            return fragmentShader;
+        }
+        private static unsafe uint BuildAndCompileVertexShader()
+        {
+            uint vertexShader = Gl.CreateShader(ShaderType.VertexShader);
+            Gl.ShaderSource(vertexShader, VertexShaderSource);
+            Gl.CompileShader(vertexShader);
+
+            //Checking the shader for compilation errors.
+            string infoLog = Gl.GetShaderInfoLog(vertexShader);
+            if (!string.IsNullOrWhiteSpace(infoLog))
+            {
+                Console.WriteLine($"Error compiling vertex shader {infoLog}");
+            }
+
+            return vertexShader;
+        }
+
+        private static unsafe void InitializeEBO()
+        {
+            //Initializing a element buffer that holds the index data.
+            Ebo = Gl.GenBuffer(); //Creating the buffer.
+            Gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, Ebo); //Binding the buffer.
+            fixed (void* i = &Indices[0])
+            {
+                Gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint)(Indices.Length * sizeof(uint)), i, BufferUsageARB.StaticDraw); //Setting buffer data.
+            }
+        }
+        private static unsafe void InitializeVBO()
+        {
+            //Initializing a vertex buffer that holds the vertex data.
+            Vbo = Gl.GenBuffer(); //Creating the buffer.
+            Gl.BindBuffer(BufferTargetARB.ArrayBuffer, Vbo); //Binding the buffer.
+            fixed (void* v = &Vertices[0])
+            {
+                Gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(Vertices.Length * sizeof(uint)), v, BufferUsageARB.StaticDraw); //Setting buffer data.
+            }
+        }
+
+        #endregion
+
+        private static void OnRender(double obj)
+        {
+            //Clear the color channel.
+            Gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+
+            //Bind the geometry and shader.
+            Gl.BindVertexArray(Vao);
+            Gl.UseProgram(Shader);
+
+            //Draw the geometry.
+            Gl.DrawElements(PrimitiveType.Triangles, (uint)Indices.Length, DrawElementsType.UnsignedInt, null);
+        }
+
+        private static void OnUpdate(double obj)
+        {
+            //Here all updates to the program should be done.
+        }
+
+        private static void OnClose()
+        {
+            //Remember to delete the buffers.
+            Gl.DeleteBuffer(Vbo);
+            Gl.DeleteBuffer(Ebo);
+            Gl.DeleteVertexArray(Vao);
+            Gl.DeleteProgram(Shader);
+        }
+
+
+        private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
+        {
+            //Check to close the window on escape.
+            if (arg2 == Key.Escape)
+            {
+                window.Close();
+            }
+        }
+    }
+}
