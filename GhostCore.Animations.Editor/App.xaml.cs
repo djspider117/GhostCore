a7fa47b34ba97f81f5690ae0773dc21715234b9f
Ionut Popovici <ionut.popovici@engageworks.com>
2021-04-24 00:39:18 +0300

    Prototyping/Designing editor

--- GhostCore.Animations.Editor/App.xaml.cs
+++ GhostCore.Animations.Editor/App.xaml.cs
@@ -66,7 +66,7 @@ namespace GhostCore.Animations.Editor
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(PrototypePage), e.Arguments);
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
